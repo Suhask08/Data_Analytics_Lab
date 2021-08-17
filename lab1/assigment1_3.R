093d9e62e37ecb0e5d6181155e483f954f586f31
Suhask08 <88901444+Suhask08@users.noreply.github.com>
2021-08-17 23:47:12 +0530

    Update assigment1_3.R
    Write the two programs in R using functions.

--- lab1/assigment1_3.R
+++ lab1/assigment1_3.R
@@ -1,1 +1,19 @@
+#WAP in R to find the highest of three numbers using function
+maximum=function(a,b,c)
+    {
+	    max=a;
+	    if(b>max)
+		    max=b;
+	    if(c>max)
+		    max=c;
+	    return(max);
+    }
 
+#WAP in R to find display the sum of n natural numbers using loop using function
+calculate_sum() <- function(n) {
+if(n <= 1) {
+return(n)
+} else {
+return(n + calculate_sum(n-1))
+}
+}
