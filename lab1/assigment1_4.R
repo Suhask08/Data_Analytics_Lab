544e7cb57bc30fd12fa5c0edba3a9fffef2a1d6b
Suhask08 <88901444+Suhask08@users.noreply.github.com>
2021-08-17 23:48:28 +0530

    Update assigment1_4.R
    WAP in R to find the multiplication table (from 1 to 10)

--- lab1/assigment1_4.R
+++ lab1/assigment1_4.R
@@ -1,1 +1,7 @@
-
+# WAP in R to find the multiplication table (from 1 to 10)
+# take input from the user
+num = as.integer(readline(prompt = "Enter a number: "))
+# use for loop to iterate 10 times
+for(i in 1:10) {
+print(paste(num,'x', i, '=', num*i))
+}
